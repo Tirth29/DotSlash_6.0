ce342e75577ba82240a9038ab3624c0998738f38
Tirth <tirthsolanki29923@gmail.com>
2023-02-18 11:58:38 +0530

    First add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App text-3xl">
+   Hello World
+    </div>
+  )
+}
+
+export default App
