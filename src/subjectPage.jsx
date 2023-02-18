0c774e6c69defc81d2c6f8056b9030244282b7c1
JayP <jaypatel4099@gmail.com>
2023-02-18 23:18:15 +0530

    subject pages

--- src/subjectPage.jsx
+++ src/subjectPage.jsx
@@ -4,9 +4,10 @@ const subjectPage = () => {
   return (
     <React.Fragment>
       <div className=' h-full w-full'>
-      <div className="mt-20 mr-0 flex-wrap inline-box p-0 flex bg-yellow-400 h-1/2 w-1/2 ">subjectPage</div>
-      <div className="m-0 mr-0 p-0 inline-box flex faculty bg-red-600 h-1/2 w-1/2 "></div>
-      <div className=" material bg-green-800 h-1/2 w-full"></div>
+        <div className='flex h-1/2 w-full'>
+          <div className="mt-20 mr-0 flex-wrap inline-box p-0 flex bg-yellow-400 w-1/2 ">subjectPage</div>
+          <div className="mt-20 mr-0 p-0 inline-box flex faculty bg-red-600  w-1/2 "> faculty</div>
+        </div><div className=" material bg-green-800 h-1/2 w-full">study material</div>
       </div>
     </React.Fragment>)
 }
