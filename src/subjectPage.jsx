043643ccd025e4af3061b2357a4ccb81addf992e
JayP <jaypatel4099@gmail.com>
2023-02-19 00:40:49 +0530

    subject css

--- src/subjectPage.jsx
+++ src/subjectPage.jsx
@@ -4,10 +4,16 @@ const subjectPage = () => {
   return (
     <React.Fragment>
       <div className=' h-full w-full'>
+        <div className='h-20'></div>
         <div className='flex h-1/2 w-full'>
-          <div className="mt-20 mr-0 flex-wrap inline-box p-0 flex bg-yellow-400 w-1/2 ">subjectPage</div>
-          <div className="mt-20 mr-0 p-0 inline-box flex faculty bg-red-600  w-1/2 "> faculty</div>
-        </div><div className=" material bg-green-800 h-1/2 w-full">study material</div>
+          <div className="mt-0 mr-0 flex-wrap inline-box p-0 flex  w-1/2 ">
+           <h1 className="inline-block text-5xl text-center bg-blue-500 mx-auto my-auto rounded-full p-10 bg-[url('./Subject-page.jpg')]">Sub-01</h1>
+
+          </div>
+          <div className="mt-0 mr-0 p-0 inline-box flex faculty   w-1/2">
+            <h1 className='inline-block text-2xl text-center mx-auto my-auto bg-blue-400 rounded-full p-10'>faculty</h1>
+          </div>
+        </div><div className=" material bg-violet-400 h-1/2 w-full text-center pt-10 mx-auto my-auto rounded-t-3xl ">study material</div>
       </div>
     </React.Fragment>)
 }
